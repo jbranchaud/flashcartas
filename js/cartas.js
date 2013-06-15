3c954147b589e02cab49e472e4b48735ebb63e6a
jbranchaud <jbranchaud@gmail.com>
2013-06-15 15:45:28 -0500

    The app now displays random words and toggles between spanish and english. [index.html] Added the click action for the flashcard and added a button for getting a random next flashcard. [css/styles.css] Adding styling to the new 'Next Word' button. [js/cartas.js] Adding the functionality to toggle between spanish and english sides of the card as well as the functionality for randomly grabbing a new word pair for the flashcard.

--- js/cartas.js
+++ js/cartas.js
@@ -5,13 +5,33 @@ flashcards = [
 ]
 
 function getRandomWord() {
-    return flashcards[0];
+    var index = getRandomInt(0, flashcards.length - 1);
+    return flashcards[index];
+}
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function AppViewModel() {
-    var spanish = getRandomWord().spanish;
-    var english = getRandomWord().english;
-    this.cardWord = spanish;
+    var currWord = getRandomWord();
+    var isSpanish = true;
+    this.cardWord = ko.observable(currWord.spanish);
+    this.toggleCardWord = function() {
+        if(isSpanish) {
+            this.cardWord(currWord.english);
+            isSpanish = false;
+        }
+        else {
+            this.cardWord(currWord.spanish);
+            isSpanish = true;
+        }
+    }
+    this.nextWord = function() {
+        currWord = getRandomWord();
+        isSpanish = true;
+        this.cardWord(currWord.spanish);
+    }
 }
 
 ko.applyBindings(new AppViewModel());
